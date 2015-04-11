3e258a9c6461e3e81f9accfd4a24a9a8396b4d60
vesloguzov <vesloguzov@gmail.com>
2015-04-11 19:39:29 +0600

    Загрузка данных из Excel
    начинает нормально работать

--- ABC-XYZ-analysis/Form1.Designer.cs
+++ ABC-XYZ-analysis/Form1.Designer.cs
@@ -28,20 +28,82 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(472, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(167, 20);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "метод 1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(454, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 38);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(627, 277);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 321);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(437, 108);
+            this.listBox1.TabIndex = 3;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(456, 322);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(153, 17);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "1 строка - наименование";
+            this.checkBox1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(503, 305);
+            this.ClientSize = new System.Drawing.Size(651, 442);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "ABC+XYZ analysis";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
     }
 }
 
--- ABC-XYZ-analysis/Form1.cs
+++ ABC-XYZ-analysis/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Reflection;
+using ExcelObj = Microsoft.Office.Interop.Excel;
 
 namespace ABC_XYZ_analysis
 {
@@ -15,6 +17,137 @@ namespace ABC_XYZ_analysis
         public Form1()
         {
             InitializeComponent();
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.DefaultExt = "*.xls;*.xlsx";
+            ofd.Filter = "Excel Sheet(*.xlsx)|*.xlsx";
+            ofd.Title = "Выберите документ для загрузки данных";
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = ofd.FileName;
+                System.IO.FileStream stream =
+                System.IO.File.Open(ofd.FileName,
+                System.IO.FileMode.Open,
+                System.IO.FileAccess.Read); //обработать исключение если файл открыт
+
+                Excel.IExcelDataReader IEDR;
+
+                int fileformat = ofd.SafeFileName.IndexOf(".xlsx");
+
+                if (fileformat > -1)
+                {
+                    //2007 format *.xlsx
+                    IEDR = Excel.ExcelReaderFactory.CreateOpenXmlReader(stream);
+                }
+                else
+                {
+                    //97-2003 format *.xls
+                    IEDR = Excel.ExcelReaderFactory.CreateBinaryReader(stream);
+                }
+
+                //Если данное значение установлено в true
+                //то первая строка используется в качестве 
+                //заголовков для колонок
+                if (checkBox1.Checked)
+                {
+                    IEDR.IsFirstRowAsColumnNames = true;
+                }
+                else {
+                    IEDR.IsFirstRowAsColumnNames = false;
+                }
+                DataSet ds = IEDR.AsDataSet();
+
+                //Устанавливаем в качестве источника данных dataset 
+                //с указанием номера таблицы. Номер таблицы указавает 
+                //на соответствующий лист в файле нумерация листов 
+                //начинается с нуля.
+                int tables_count = ds.Tables.Count;
+                listBox1.Items.Add( "Число страниц: "+tables_count.ToString());
+                for (int i = 0; i < tables_count; i++)
+                {
+                    listBox1.Items.Add("Имя "+i+"й "+"таблицы: "+ ds.Tables[i].TableName);
+                }
+
+                    dataGridView1.DataSource = ds.Tables[0];
+                IEDR.Close();
+            }
+            else
+            {
+                MessageBox.Show("Вы не выбрали файл для открытия",
+                 "Загрузка данных...", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            //Задаем расширение имени файла по умолчанию.
+            ofd.DefaultExt = "*.xls;*.xlsx";
+            //Задаем строку фильтра имен файлов, которая определяет
+            //варианты, доступные в поле "Файлы типа" диалогового
+            //окна.
+            ofd.Filter = "Excel Sheet(*.xlsx)|*.xlsx";
+            //Задаем заголовок диалогового окна.
+            ofd.Title = "Выберите документ для загрузки данных";
+            ExcelObj.Application app = new ExcelObj.Application();
+            ExcelObj.Workbook workbook;
+            ExcelObj.Worksheet NwSheet;
+            ExcelObj.Range ShtRange;
+            DataTable dt = new DataTable();
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = ofd.FileName;
+
+                workbook = app.Workbooks.Open(ofd.FileName, Missing.Value,
+                Missing.Value, Missing.Value, Missing.Value, Missing.Value,
+                Missing.Value, Missing.Value, Missing.Value, Missing.Value,
+                Missing.Value, Missing.Value, Missing.Value, Missing.Value,
+                Missing.Value);
+
+                //Устанавливаем номер листа из котрого будут извлекаться данные
+                //Листы нумеруются от 1
+                NwSheet = (ExcelObj.Worksheet)workbook.Sheets.get_Item(1);
+                ShtRange = NwSheet.UsedRange;
+                for (int Cnum = 1; Cnum <= ShtRange.Columns.Count; Cnum++)
+                {
+                    dt.Columns.Add(
+                       new DataColumn((ShtRange.Cells[1, Cnum] as ExcelObj.Range).Value2.ToString()));
+                }
+                dt.AcceptChanges();
+
+                string[] columnNames = new String[dt.Columns.Count];
+                for (int i = 0; i < dt.Columns.Count; i++)
+                {
+                    columnNames[0] = dt.Columns[i].ColumnName;
+                }
+
+                for (int Rnum = 2; Rnum <= ShtRange.Rows.Count; Rnum++)
+                {
+                    DataRow dr = dt.NewRow();
+                    for (int Cnum = 1; Cnum <= ShtRange.Columns.Count; Cnum++)
+                    {
+                        if ((ShtRange.Cells[Rnum, Cnum] as ExcelObj.Range).Value2 != null)
+                        {
+                            dr[Cnum - 1] =
+                (ShtRange.Cells[Rnum, Cnum] as ExcelObj.Range).Value2.ToString();
+                        }
+                    }
+                    dt.Rows.Add(dr);
+                    dt.AcceptChanges();
+                }
+
+                dataGridView1.DataSource = dt;
+                app.Quit();
+            }
+            else
+                Application.Exit();
         }
     }
 }
